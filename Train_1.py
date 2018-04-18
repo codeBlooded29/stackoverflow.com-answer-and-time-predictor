db741a66efa77fd14355b813ab11d7448ae57002
Siddharth Thakur <codeblooded29@gmail.com>
2018-04-18 14:49:32 +0530

    Training using Support Vector Classifier

--- Train_1.py
+++ Train_1.py
@@ -4,18 +4,25 @@ import numpy as np
 from sklearn.externals import joblib
 
 
-with open('features_train', 'rb') as fp:
+with open('features_train1', 'rb') as fp:
     features_train = pickle.load(fp)
-with open('labels_train', 'rb') as fp:
+with open('labels_train1', 'rb') as fp:
     labels_train = pickle.load(fp)
 
-x = np.array(features_train)
-y = np.array(labels_train)
-clf = SVC(kernel='rbf')
-clf.fit(x,y)
+X_train = np.array(features_train)
+y_train = np.array(labels_train)
+
+
+clf = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+    decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
+    max_iter=-1, probability=False, random_state=None, shrinking=True,
+    tol=0.001, verbose=False)
+clf.fit(X_train,y_train)
 
 joblib.dump(clf,'trained_1.pkl')
-print('successful')
 
-res = clf.score(x,y)
+#print(res.tolist().count(1))
+#print(labels_test.count(1))
+res = clf.score(X_train,y_train)
 print(res)
+print('Training Successful!')
