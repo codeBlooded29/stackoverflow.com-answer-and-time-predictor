05e8eebbdc211357534373d9f48aeacbbc01e7fc
Siddharth Thakur <codeblooded29@gmail.com>
2018-04-18 14:52:07 +0530

    Training using Decision Tree Classifier
    It will also return mean accuracy score on testing data

--- Train_2.py
+++ Train_2.py
@@ -2,22 +2,24 @@ import pickle
 from sklearn import tree
 import numpy as np
 from sklearn.externals import joblib
-from sklearn.ensemble import RandomForestClassifier
 
-with open('features_train', 'rb') as fp:
+
+with open('features_train1', 'rb') as fp:
     features_train = pickle.load(fp)
-with open('labels_train', 'rb') as fp:
+with open('labels_train1', 'rb') as fp:
     labels_train = pickle.load(fp)
 
 print(len(features_train))
 print(len(labels_train))
-x = np.array(features_train)
-y = np.array(labels_train)
-clf = RandomForestClassifier()
-clf.fit(x,y,sample_weight=None)
+
+X_train = np.array(features_train)
+y_train = np.array(labels_train)
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(X_train,y_train)
 
 joblib.dump(clf,'trained_2.pkl')
-print('successful')
 
-res = clf.score(x,y)
+res = clf.score(X_train,y_train)
 print(res)
+print('Training Successful!')
